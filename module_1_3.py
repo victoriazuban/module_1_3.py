9324b6595da637cd01590dccdd287fdc374fcfe2
victoriazuban <victoriazyuban@icloud.com>
2024-12-07 16:36:57 +0200

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,18 @@
+
+#module_1_3.py
+name = 'victoria'
+print (name)
+age = 22
+print (age)
+print (age + 5)
+age +=15
+print (age)
+age += 17
+print (age)
+true = 'is_student'
+print (true)
+is_student = 'true'
+print (is_student)
+print (true + is_student)
+
+
